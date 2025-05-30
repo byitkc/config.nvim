744eeeedb27d095020941bd5e5d1ae5d7083725c
Brandon Young <brandon@byitkc.com>
2025-05-30 09:37:48 -0500

    Adjusting go autoformat

--- lua/config/autocmd/go.lua
+++ lua/config/autocmd/go.lua
@@ -3,9 +3,25 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   group = vim.api.nvim_create_augroup("group1", { clear = true }),
   pattern = "*.go",
   callback = function()
-    local working_file = vim.fn.expand("%:p")
-    local command = { "goimports", "-w", working_file }
-    os.execute(vim.fn.join(command, " "))
-    vim.cmd.edit(working_file)
+    local filepath = vim.fn.expand("%:p")
+
+    -- If the file is a `templ` generate `.go` file we will skip `goimports` as it
+    -- overrides the templ formatting from `templ generate` on the file.
+    local path_table = vim.fn.split(filepath, "/")
+    local filename = path_table[#path_table]
+    local subfile = string.find(filename, "%w_templ.go")
+    if subfile ~= nil then
+      return
+    end
+
+    -- Just incase there is a `*.go` file that isn't detected as `go`.
+    if vim.bo.filetype ~= "go" then
+      return
+    end
+
+    -- Execute `goimport` on the file and overwrite it.
+    os.execute(vim.fn.join({ "goimports", "-w", filepath }, " "))
+    -- "Re-edit" the file to update the buffer with the post `goimprts` file.
+    vim.cmd.edit(filepath)
   end,
 })

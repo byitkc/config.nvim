557bb898d9fc300eb4ce7304a64c904c2fa45436
Brandon Young <brandon@byitkc.com>
2025-06-03 02:52:13 -0500

    changing theme to 'night' variant

--- lua/config/plugins/tokyonight.lua
+++ lua/config/plugins/tokyonight.lua
@@ -5,7 +5,8 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      vim.cmd.colorscheme "tokyonight-moon"
+      -- vim.cmd.colorscheme "tokyonight-moon"
+      vim.cmd.colorscheme "tokyonight-night"
     end,
   },
 }

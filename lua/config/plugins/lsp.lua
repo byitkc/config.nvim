9c0a4fce9c2859be28b4ae69d4fbc611f404bc7d
Brandon Young <brandon@byitkc.com>
2025-07-02 06:03:38 -0500

    adding keymap to go to definition

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -33,6 +33,7 @@ return {
       --   provideFormatter = false,
       -- }
       -- require("lspconfig").html.setup(opts)
+      vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('my.lsp', {}),

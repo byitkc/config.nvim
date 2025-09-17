13f3a555b9d30f53f3c01532f43a44d2bb009692
Brandon Young <brandon@byitkc.com>
2025-09-17 07:49:21 -0500

    changing theme to match wezterm theme

--- lua/config/plugins/catppuccin.lua
+++ lua/config/plugins/catppuccin.lua
@@ -1,12 +1,11 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    opts = {},
     config = function()
       -- vim.cmd.colorscheme "tokyonight-moon"
-      vim.cmd.colorscheme "tokyonight-night"
+      vim.cmd.colorscheme "catppuccin"
     end,
-  },
+  }
 }

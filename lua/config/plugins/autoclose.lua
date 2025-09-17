64ecaa06c4ff90d40272499d6d972655b1557d7c
Brandon Young <brandon@byitkc.com>
2025-09-17 07:49:32 -0500

    removed autoclose for double quotes

--- lua/config/plugins/autoclose.lua
+++ lua/config/plugins/autoclose.lua
@@ -2,11 +2,12 @@
 return {
   {
     "m4xshen/autoclose.nvim",
-    enabled = false,
     config = function()
       require("autoclose").setup({
         keys = {
-          ["'"] = { escape = false, close = false, pair = "''", disabled_filetypes = {} }
+          ["'"] = { escape = false, close = false, pair = "''", disabled_filetypes = {} },
+          ["`"] = { escape = false, close = false, pair = "``", disabled_filetypes = {} },
+          ["\""] = { escape = false, close = false, pair = "\"\"", disabled_filetypes = {} }
         }
       })
     end,
